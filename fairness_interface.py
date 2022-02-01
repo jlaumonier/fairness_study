466b1ec8d26f5834d5db1e6f6fcedcdbb39498da
julien Laumonier <laumonierjulien@gmail.com>
2022-02-01 15:52:45 -0500

    Initial commit

---
+++ fairness_interface.py
@@ -0,0 +1,210 @@
+import shap
+import numpy as np
+import pandas as pd
+import sklearn
+import streamlit as st
+
+import plotly.graph_objects as go
+
+from fairness_utils import FairnessUtils
+
+
+def create_dataframe(x, y_gold, base_columns, y_pred=None):
+    if y_pred is not None:
+        data_array = np.concatenate((x, np.array([y_gold]).T, np.array([y_pred]).T), axis=1)
+    else:
+        data_array = np.concatenate((x, np.array([y_gold]).T), axis=1)
+    columns = list(base_columns).copy()
+    columns.append('target')
+    if y_pred is not None:
+        columns.append('target_pred')
+    df = pd.DataFrame(data=data_array, columns=columns)
+    return df
+
+
+def add_priv_class_col(df, column, condition):
+    # Adding a priv_clas
+    df['priv_class'] = df[column].map(eval('lambda x:' + condition))
+    return df
+
+
+def recalculate_from_demographic_parity(df):
+    def change_f(delta):
+        change_f = [0.0, 1.0] if delta < 0 else [1.0, 0.0]
+        nb_priv_false = len(df[df['priv_class'] == False])
+        # change F
+        diff_nb_f = delta * nb_priv_false
+        int_diff_nb_f = int(diff_nb_f)
+        df_f_condition = df[(df['target_pred'] == change_f[0]) & (df['priv_class'] == False)]
+        nb_f = min(abs(int_diff_nb_f), len(df_f_condition))
+        st.text('  F ' + str(change_f[0]) + ' -> ' + str(change_f[1]) + ' to change ' + str(nb_f) + '/' + str(
+            len(df_f_condition)))
+        random_df_f_condition = df_f_condition.sample(n=abs(nb_f))
+        for idx in random_df_f_condition.index:
+            df.iloc[idx, df.columns.get_loc('target_pred')] = change_f[1]
+
+    def change_t(delta):
+        change_t = [1.0, 0.0] if delta < 0 else [0.0, 1.0]
+        nb_priv_true = len(df[df['priv_class'] == True])
+        # change T
+        diff_nb_t = - delta * nb_priv_true
+        df_t_condition = df[(df['target_pred'] == change_t[0]) & (df['priv_class'] == True)]
+        int_diff_t = int(diff_nb_t)
+        nb_t = min(abs(int_diff_t), len(df_t_condition))
+        st.text('  T ' + str(change_t[0]) + ' -> ' + str(change_t[1]) + ' to change ' + str(nb_t) + '/' + str(
+            len(df_t_condition)))
+        random_df_t_1_condition = df_t_condition.sample(n=abs(nb_t))
+        for idx in random_df_t_1_condition.index:
+            df.iloc[idx, df.columns.get_loc('target_pred')] = change_t[1]
+
+    st.text('Recalculate from demographic parity')
+    delta_dp = old_demographic_parity - wanted_demographic_parity
+    delta_dp_f = delta_dp * correction_priority
+    delta_dp_t = delta_dp * (1 - correction_priority)
+    st.text('  delta_dp_f ' + str(delta_dp_f))
+    st.text('  delta_dp_t ' + str(delta_dp_t))
+
+    change_f(delta_dp_f)
+    change_t(delta_dp_t)
+
+    new_dp = FairnessUtils.demographic_parity(df)
+    delta_dp = new_dp - wanted_demographic_parity
+    st.text('  After first changes, diff ' + str(delta_dp))
+
+    # T is entirely used -> change F
+    change_f(delta_dp)
+    new_dp = FairnessUtils.demographic_parity(df)
+    delta_dp = new_dp - wanted_demographic_parity
+    st.text('  After change F, diff ' + str(delta_dp))
+    change_t(delta_dp)
+    new_dp = FairnessUtils.demographic_parity(df)
+    delta_dp = new_dp - wanted_demographic_parity
+    st.text('  After change T, diff ' + str(delta_dp))
+
+    return df
+
+
+def create_accuracy_figure(data, metrics):
+    fig = go.Figure()
+
+    # Add graph data
+    olds = [d[0] for d in data]
+    wanteds = [d[1] for d in data]
+    news = [d[2] for d in data]
+
+    # Make traces for graph
+    trace1 = go.Bar(x=metrics, y=olds, xaxis='x2', yaxis='y2',
+                    marker=dict(color='#0099ff'),
+                    name='Before')
+    trace2 = go.Bar(x=metrics, y=wanteds, xaxis='x2', yaxis='y2',
+                    marker=dict(color='#ff9900'),
+                    name='Wanted')
+    trace3 = go.Bar(x=metrics, y=news, xaxis='x2', yaxis='y2',
+                    marker=dict(color='#404040'),
+                    name='After')
+
+    # Add trace data to figure
+    fig.add_traces([trace1, trace2, trace3])
+    return fig
+
+
+x, y = shap.datasets.adult()
+x_display, y_display = shap.datasets.adult(display=True)
+x_train, x_valid, y_train, y_valid = sklearn.model_selection.train_test_split(x, y, test_size=0.2, random_state=7)
+
+prediction = st.selectbox('Prediction',
+                          ['Gold', 'KNN', 'AdaBoost', 'SVM'],
+                          index=0)
+
+if prediction != 'Gold':
+    model = None
+    if prediction == 'KNN':
+        model = sklearn.neighbors.KNeighborsClassifier()
+    if prediction == 'SVM':
+        model = sklearn.svm.SVC()
+    if prediction == 'AdaBoost':
+        model = sklearn.ensemble.AdaBoostClassifier(n_estimators=100)
+    model.fit(x_train, y_train)
+    y_prediction = model.predict(x_valid)
+    y_prediction_all = model.predict(x)
+else:
+    y_prediction = y_valid.copy()
+    y_prediction_all = y.copy()
+
+data_df_display = create_dataframe(x_display, y_display, x_display.columns)
+st.dataframe(data_df_display)
+
+fairness_mesured_set = st.selectbox('Fairness mesured set', ['All', 'Valid'], index=1)
+
+data_df = None
+if fairness_mesured_set == 'All':
+    data_df = create_dataframe(x=x, y_gold=y, base_columns=x_display.columns, y_pred=y_prediction_all)
+if fairness_mesured_set == 'Valid':
+    data_df = create_dataframe(x=x_valid, y_gold=y_valid, base_columns=x_display.columns, y_pred=y_prediction)
+
+privilege_col = st.selectbox('discrimination column',
+                             list(x_display.columns),
+                             index=7)
+condition_str = st.text_input('condition for privilege class', value='x==1')
+
+if condition_str is not None:
+    data_df = add_priv_class_col(data_df, privilege_col, condition_str)
+
+st.dataframe(data_df)
+
+gold_demographic_parity = FairnessUtils.demographic_parity(data_df, target='target')
+old_demographic_parity = FairnessUtils.demographic_parity(data_df)
+old_disparate_impact = FairnessUtils.disparate_impact_rate(data_df)
+old_equal_opportunity_succ = FairnessUtils.equal_opportunity_succes(data_df)
+old_avg_equalized_odds = FairnessUtils.average_equalized_odds(data_df)
+old_predictive_rate_parity = FairnessUtils.predictive_rate_parity(data_df)
+
+accuracy_before = sklearn.metrics.accuracy_score(y_true=data_df['target'].tolist(),
+                                                 y_pred=data_df['target_pred'].tolist())
+
+st.subheader('Desired metrics')
+
+wanted_demographic_parity = st.slider('Demographic Parity', min_value=-1.0, max_value=1.0, step=0.01,
+                                      value=old_demographic_parity)
+
+correction_priority = st.slider('Priorize Non privilege', min_value=0.0, max_value=1.0, step=0.1,
+                                value=1.0)
+
+# wanted_equal_opportunity = st.slider('Equal Oportunity', min_value=-1.0, max_value=1.0, step=0.01,
+#                                      value=old_equal_opportunity)
+
+st.subheader('Recalculate')
+data_df = recalculate_from_demographic_parity(data_df)
+new_demographic_parity = FairnessUtils.demographic_parity(data_df)
+new_disparate_impact = FairnessUtils.disparate_impact_rate(data_df)
+new_equal_opportunity_succ = FairnessUtils.equal_opportunity_succes(data_df)
+new_avg_equalized_odds = FairnessUtils.average_equalized_odds(data_df)
+new_predictive_rate_parity = FairnessUtils.predictive_rate_parity(data_df)
+
+accuracy_after = sklearn.metrics.accuracy_score(y_true=data_df['target'].tolist(),
+                                                y_pred=data_df['target_pred'].tolist())
+
+data = [[accuracy_before, 1.0, accuracy_after],
+        [old_demographic_parity, wanted_demographic_parity, new_demographic_parity],
+        [old_equal_opportunity_succ, 0.0, new_equal_opportunity_succ],
+        [old_avg_equalized_odds, 0.0, new_avg_equalized_odds],
+        [old_predictive_rate_parity, 0.0, new_predictive_rate_parity]]
+fig_res = create_accuracy_figure(data=data, metrics=['Accuracy',
+                                                     'Demographic parity',
+                                                     'Equal Opportunity (succes)',
+                                                     'Avg Equalized Odds',
+                                                     'Predictive rate parity'])
+
+data = [[old_disparate_impact, 1.0, new_disparate_impact]]
+fig_ratio = create_accuracy_figure(data=data, metrics=['Disparate Impact'])
+
+st.header('Results')
+st.write('Demographic parity on gold :', gold_demographic_parity)
+st.write('Demographic parity :', new_demographic_parity)
+st.write('Disparate impact:', new_disparate_impact)
+st.write('Equal Opportunity (succes):', new_equal_opportunity_succ)
+st.write('Avg Equalized Odds:', new_avg_equalized_odds)
+st.write('Predictive rate parity', new_predictive_rate_parity)
+st.plotly_chart(fig_res, use_container_width=True)
+st.plotly_chart(fig_ratio, use_container_width=True)
+
---
+++ fairness_utils.py
@@ -0,0 +1,109 @@
+import operator
+from functools import reduce
+from operator import eq
+
+
+class FairnessUtils:
+
+    @classmethod
+    def calculate_cond_probability(cls, df, ofs, givens):
+        """ calculate conditional probability of 'ofs' given 'givens' """
+        givens_conditions = []
+        ofs_conditions = []
+        for of in ofs:
+            ofs_conditions.append(eq(df[of[0]], of[1]))
+        for given in givens:
+            givens_conditions.append(eq(df[given[0]], given[1]))
+            ofs_conditions.append(eq(df[given[0]], given[1]))
+
+        ofs_logical_conditions = reduce(operator.and_, ofs_conditions)
+        givens_logical_conditions = reduce(operator.and_, givens_conditions)
+
+        set_ofs_inter_givens = df.loc[ofs_logical_conditions]
+        card_ofs_inter_givens = len(set_ofs_inter_givens.index)
+        set_givens = df.loc[givens_logical_conditions]
+        card_givens = len(set_givens.index)
+        result = card_ofs_inter_givens / card_givens
+        return result
+
+    @classmethod
+    def demographic_parity(cls, df, target='target_pred'):
+        # Demographic parity
+        # P(Y=1 | Priv_class = False) - P(Y=1 | Priv_class = True)
+
+        false_proba = FairnessUtils.calculate_cond_probability(df, [(target, 1)], [('priv_class', False)])
+        true_proba = FairnessUtils.calculate_cond_probability(df, [(target, 1)], [('priv_class', True)])
+        demographic_parity = false_proba - true_proba
+        return demographic_parity
+
+    @classmethod
+    def disparate_impact_rate(cls, df):
+        # Disparate impact ratio
+        # P(Ŷ=1 | Priv_class = False) / P(Ŷ=1 | Priv_class = True)
+        false_proba = FairnessUtils.calculate_cond_probability(df, [('target_pred', 1)], [('priv_class', False)])
+        true_proba = FairnessUtils.calculate_cond_probability(df, [('target_pred', 1)], [('priv_class', True)])
+        if true_proba != 0.0:
+            disparate_impact_rate = false_proba / true_proba
+        else:
+            disparate_impact_rate = None
+        return disparate_impact_rate
+
+    @classmethod
+    def equal_opportunity_error(cls, df):
+        # equal opportunity
+        # P(Y_hat=0 | Y=1, Priv_class = False) - P(Y_hat=0 | Y=1, Priv_class = True)
+        false_proba = FairnessUtils.calculate_cond_probability(df, [('target_pred', 0)],
+                                                               [('target', 1),
+                                                                ('priv_class', False)])
+        true_proba = FairnessUtils.calculate_cond_probability(df, [('target_pred', 0)],
+                                                              [('target', 1),
+                                                               ('priv_class', True)])
+        equal_opportunity = false_proba - true_proba
+        return equal_opportunity
+
+    @classmethod
+    def equal_opportunity_succes(cls, df):
+        # equal opportunity
+        # P(Y_hat=1 | Y=1, Priv_class = False) - P(Y_hat=1 | Y=1, Priv_class = True)
+        false_proba = FairnessUtils.calculate_cond_probability(df, [('target_pred', 1)],
+                                                               [('target', 1),
+                                                                ('priv_class', False)])
+        true_proba = FairnessUtils.calculate_cond_probability(df, [('target_pred', 1)],
+                                                              [('target', 1),
+                                                               ('priv_class', True)])
+        equal_opportunity = false_proba - true_proba
+        return equal_opportunity
+
+    @classmethod
+    def average_equalized_odds(cls, df):
+        # average equalized odds
+        # Sum_i\inI [P(Y_hat=1 | Y=i, Priv_class = False) - P(Y_hat=1 | Y=i, Priv_class = True)] / |I|
+        diff = 0
+        for i in [0, 1]:
+            false_proba = FairnessUtils.calculate_cond_probability(df,
+                                                                   [('target_pred', 1)],
+                                                                   [('target', i),
+                                                                    ('priv_class', False)])
+            true_proba = FairnessUtils.calculate_cond_probability(df,
+                                                                  [('target_pred', 1)],
+                                                                  [('target', i),
+                                                                   ('priv_class', True)])
+            diff = diff + (false_proba - true_proba)
+        average_equalized_odds = diff / 2.0
+        return average_equalized_odds
+
+    @classmethod
+    def predictive_rate_parity(cls, df):
+        # predictive rate parity
+        # P(Y=1|Y_hat=1, Priv_class=False) - P(Y=1|Y_hat=1, Priv_class=True)
+        predictive_rate_parity = None
+        if ((len(df[(df['target_pred'] == 1) & (df['priv_class'] == False)]) != 0) and
+                (len(df[(df['target_pred'] == 1) & (df['priv_class'] == True)]) != 0)):
+            false_proba = FairnessUtils.calculate_cond_probability(df, [('target', 1)],
+                                                                   [('target_pred', 1),
+                                                                    ('priv_class', False)])
+            true_proba = FairnessUtils.calculate_cond_probability(df, [('target', 1)],
+                                                                  [('target_pred', 1),
+                                                                   ('priv_class', True)])
+            predictive_rate_parity = false_proba - true_proba
+        return predictive_rate_parity
